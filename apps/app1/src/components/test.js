d674dcc8c20550290f6ecb508175afb5d5adb023
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-08-20 10:19:14 +0200

    Create reproduction

--- apps/app1/src/components/test.js
+++ apps/app1/src/components/test.js
@@ -1,1 +1,4 @@
-// Can't import "someVar" here, it never finds it.
+// Try to import "someVar" here, it only suggests the relative import
+// because of `.vscode/settings.json`.
+// I believe it would make more sense to offer the absolute path, since the 
+// module is declared in tsconfig.json "paths"
